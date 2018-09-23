3a9a5d4953910c2d4efe81c9469a1db59743d761
JAWilmer <39922930+JAWilmer@users.noreply.github.com>
2018-09-23 13:05:55 -0500

    Add files via upload

--- assets/travel.js
+++ assets/travel.js
@@ -1,4 +1,3 @@
-$(document).ready(function(){
 
   let minDate = new Date();
 
@@ -19,7 +18,6 @@ $(document).ready(function(){
           $('#depart').datepicker('option', 'minDate', 'selectedDate');
   }
 });
-});
 
 //MAP SCRIPT
 var endLocationLat;
@@ -33,15 +31,61 @@ var polyline = null;
 var gmarkers = [];
 var infowindow = new google.maps.InfoWindow();
 
+
+
+  //    // Note: This example requires that you consent to location sharing when
+  //    // prompted by your browser. If you see the error "The Geolocation service
+  //    // failed.", it means you probably did not give permission for the browser to
+  //    // locate you.
+  //    var map, infoWindow;
+  //    function initMap() {
+  //      map = new google.maps.Map(document.getElementById('map'), {
+  //        center: {lat: -34.397, lng: 150.644},
+  //        zoom: 6
+  //      });
+  //      infoWindow = new google.maps.InfoWindow;
+
+  //      // Try HTML5 geolocation.
+  //      if (navigator.geolocation) {
+  //        navigator.geolocation.getCurrentPosition(function(position) {
+  //          var pos = {
+  //            lat: position.coords.latitude,
+  //            lng: position.coords.longitude
+  //          };
+
+  //          infoWindow.setPosition(pos);
+  //          infoWindow.setContent('Location found.');
+  //          infoWindow.open(map);
+  //          map.setCenter(pos);
+  //        }, function() {
+  //          handleLocationError(true, infoWindow, map.getCenter());
+  //        });
+  //      } else {
+  //        // Browser doesn't support Geolocation
+  //        handleLocationError(false, infoWindow, map.getCenter());
+  //      }
+  //    }
+
+  //    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  //      infoWindow.setPosition(pos);
+  //      infoWindow.setContent(browserHasGeolocation ?
+  //                            'Error: The Geolocation service failed.' :
+  //                            'Error: Your browser doesn\'t support geolocation.');
+  //      infoWindow.open(map);
+  //    }
+  //  </script>
+
+
+
 // INITIALIZE MAP AND SET OPTIONS
 function initMap() {
   var directionsService = new google.maps.DirectionsService;
   var directionsDisplay = new google.maps.DirectionsRenderer;
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 7,
-    center: { // Set to Austin
-      lat: 30.2672,
-      lng: -97.7431
+    center: { // Set to Australia
+      lat: -34.397,
+      lng: 150.644
     }
   });
   //Create and control polyline
