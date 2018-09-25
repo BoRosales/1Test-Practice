6ced0389f3c9b1b504c8671ca860342d6f2c86b3
huesos <bonirosales@gmail.com>
2018-09-24 19:46:20 -0500

    copy from Develop

--- assets/travel.js
+++ assets/travel.js
@@ -1,122 +1,241 @@
-//---Call mygasFeed---
-var gasLat = 30.2672; // will call from google
-var gasLng = -97.7431; // will call from google
-var queryURL = "http:/api.mygasfeed.com/stations/radius/" + gasLat + "/" + gasLng + "/5/reg/price/esmbi7wobr.json?";
 
+  let minDate = new Date();
 
-$.get(queryURL).then( data => {
-    console.log(JSON.stringify(data));
-    $(".gasStuff").append("<tr><td>" + data.stations[0].station + "</td><td>" + "$" + data.stations[0].reg_price + "</td><td>" + data.stations[0].address + "</td><td>" + data.stations[0].distance + "</td></tr>")
-    //$("#info").text(JSON.stringify(data.stations[0].reg_price));
-    //console.log("country" + data.stations.zip);
+  $('#depart').datepicker({
+      showAnim: 'drop',
+      numberOfMonth: 1,
+      minDate: minDate,
+      dateFormat: 'mm/dd/yy',
+      onClose: function(selectedDate){
+          $('#return').datepicker('option', 'minDate', selectedDate);
+      }
 });
-//---Input Variables---
-var input1 = document.getElementById("origin");
-var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
-var input2 = document.getElementById("destination");
-var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
+  $('#return').datepicker({
+      showAnim: 'drop',
+      numberOfMonth: 1,
+      minDate: new Date,
+      dateFormat: 'mm/dd/yy',
+      onClose: function(selectedDate){
+          $('#depart').datepicker('option', 'minDate', selectedDate);
+  }
+});
 
-//---Long and Lat global variables---
+//MAP SCRIPT
 var endLocationLat;
 var endLocationLng;
 var startLocationLat;
 var startLocationLng;
+var directionDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+var polyline = null;
+var gmarkers = [];
+var infowindow = new google.maps.InfoWindow();
 
-//---MAP SCRIPT---
-//set options
-var myLatLng = {
-    lat: 30.2672,
-    lng: -97.7431
-}; // Set to Austin, could set to user's location
-var mapOptions = {
-    center: myLatLng,
+// INITIALIZE MAP AND SET OPTIONS
+function initMap() {
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 7,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    center: { // Set to Austin
+      lat: 30.2672,
+      lng: -97.7431
+    }
+  });
+  //Create and control polyline
+  polyline = new google.maps.Polyline({
+    path: [],
+    strokeColor: '#FF0000',
+    strokeWeight: 3
+  });
+  // Calculate and display routes
+  directionsDisplay.setMap(map);
+  calculateAndDisplayRoute(directionsService, directionsDisplay);
+  var onChangeHandler = function () {
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+  document.getElementById('searchAround').addEventListener('click', onChangeHandler);
+}
+
+//CALUCLATE AND DISPLAY ROUTE
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  directionsService.route({
+    origin: document.getElementById('origin').value,
+    destination: document.getElementById('destination').value,
+    travelMode: 'DRIVING'
+  },
+    function (response, status) {
+      if (status == google.maps.DirectionsStatus.OK) {
+        polyline.setPath([]);
+        var bounds = new google.maps.LatLngBounds();
+        startLocation = new Object();
+        endLocation = new Object();
+        directionsDisplay.setDirections(response);
+        var route = response.routes[0];
+        var path = response.routes[0].overview_path;
+        // console.log("overview path lat/ long coordinates: "+ response.routes[0].overview_path);
+        var legs = response.routes[0].legs;
+        //Display distance and time in DOM
+        $("#output").html("<div class='alert-info'>From: " + document.getElementById("origin").value + ".<br />To: " + document.getElementById("destination").value + ".<br /> Driving distance: " + response.routes[0].legs[0].distance.text + ".<br />Duration: " + response.routes[0].legs[0].duration.text + ".</div>");
+        // startLocationLat = response.routes[0].legs[0].start_location.lat();
+        // startLocationLng = response.routes[0].legs[0].start_location.lng();
+        // endLocationLat = response.routes[0].legs[0].end_location.lat();
+        // endLocationLng = response.routes[0].legs[0].end_location.lng();
+        // console.log("Origin Latitude: " + startLocationLat);
+        // console.log("Origin Longitude: " + startLocationLng);
+        // console.log("Destination Latitude: " + endLocationLat);
+        // console.log("Destination Longitude: " + endLocationLng);
+
+        for (i = 0; i < legs.length; i++) {
+          if (i == 0) {
+            startLocation.latlng = legs[i].start_location;
+            startLocation.address = legs[i].start_address;
+            // marker = google.maps.Marker({map:map,position: startLocation.latlng});
+            // marker = createMarker(legs[i].start_location, "start", legs[i].start_address, "green");
+          }
+          endLocation.latlng = legs[i].end_location;
+          endLocation.address = legs[i].end_address;
+          var steps = legs[i].steps;
+          for (j = 0; j < steps.length; j++) {
+            var nextSegment = steps[j].path;
+            for (k = 0; k < nextSegment.length; k++) {
+              polyline.getPath().push(nextSegment[k]);
+              bounds.extend(nextSegment[k]);
+            }
+          }
+        }
+
+        polyline.setMap(map);
+        for (var i = 0; i < gmarkers.length; i++) {
+          gmarkers[i].setMap(null);
+        }
+        gmarkers = [];
+        //321100 = about 200 miles
+        var points = polyline.GetPointsAtDistance(321100);
+        for (var i = 0; i < points.length; i++) {
+          var marker = new google.maps.Marker({
+            map: map,
+            position: points[i],
+            title: i*200 + 200 + " miles"
+          });
+          marker.addListener('click', openInfoWindow);
+        }
+      }
+    });
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
+
+// CREATE 200 MILE MARKERS
+function createMarker(latlng, label, html, color) {
+  var contentString = '<b>' + label + '</b><br>' + html;
+  var marker = new google.maps.Marker({
+    position: latlng,
+    // draggable: true, // this part is not working correctly
+    map: map,
+    icon: getMarkerImage(color),
+    title: label,
+    zIndex: Math.round(latlng.lat() * -100000) << 5
+  });
+  marker.myname = label;
+  gmarkers.push(marker);
+
+  google.maps.event.addListener(marker, 'click', function () {
+    infowindow.setContent(contentString);
+    infowindow.open(map, marker);
+  });
+  return marker;
+}
+var icons = new Array();
+icons["red"] = {
+  url: "http://maps.google.com/mapfiles/ms/micons/red.png"
 };
 
-//Create map
-var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+// DISPLAY MARKERS ON MAP
+function getMarkerImage(iconColor) {
+  if ((typeof (iconColor) == "undefined") || (iconColor == null)) {
+    iconColor = "red";
+  }
+  if (!icons[iconColor]) {
+    icons[iconColor] = {
+      url: "http://maps.google.com/mapfiles/ms/micons/" + iconColor + ".png"
+    };
+  }
+  return icons[iconColor];
 
-//Create DirectionsService and DirectionsRender objects, bind DirectionsRenderer to map
-var directionsService = new google.maps.DirectionsService();
-var directionsDisplay = new google.maps.DirectionsRenderer();
+}
+// SHOW LAT/ LONG AND DISTANCE FROM ORIGIN FOR EACH 200 MI POINT
+function openInfoWindow() {
+  var contentString = this.getTitle() + "<br>" + this.getPosition().toUrlValue(6);
+  infowindow.setContent(contentString);
+  infowindow.open(map, this);
+}
 
-directionsDisplay.setMap(map);
-
-//Calculate route function
-function calcRoute() {
-    var request = {
-        origin: document.getElementById("origin").value,
-        destination: document.getElementById("destination").value,
-        travelMode: google.maps.TravelMode.DRIVING,
-        unitSystem: google.maps.UnitSystem.IMPERIAL
-        ////////////////////////////////////////////// We could add "waypoints" or "avoid tolls/ highways" optons here
+//Uses a method to return an array of lat/ long points at given intervals along route
+google.maps.Polyline.prototype.GetPointsAtDistance = function (miles) {
+  var next = miles;
+  var points = [];
+  // Special cases
+  if (miles <= 0) return points;
+  var dist = 0;
+  var olddist = 0;
+  for (var i = 1;
+    (i < this.getPath().getLength()); i++) {
+    olddist = dist;
+    dist += google.maps.geometry.spherical.computeDistanceBetween(this.getPath().getAt(i), this.getPath().getAt(i - 1));
+    while (dist > next) {
+      var p1 = this.getPath().getAt(i - 1);
+      var p2 = this.getPath().getAt(i);
+      var m = (next - olddist) / (dist - olddist);
+      points.push(new google.maps.LatLng(p1.lat() + (p2.lat() - p1.lat()) * m, p1.lng() + (p2.lng() - p1.lng()) * m));
+      next += miles;
+      /////////HHHEEEEEERRRRRRRRREEEEEE ARE THE LAT/ LONG COORDINATES YOU NEED FOR THE GAS API
+      console.log (p1.lat() + (p2.lat() - p1.lat()) * m, p1.lng() + (p2.lng() - p1.lng()) * m)
     }
+  }
 
-    //Pass request to route method
-    directionsService.route(request, function (result, status) {
-        if (status == google.maps.DirectionsStatus.OK) {
-            console.log(result.routes[0].legs[0].distance);
-            //Get distance, time, and lat/ long.  Display distance and time in DOM
-            $("#output").html("<div class='alert-info'>From: " + document.getElementById("origin").value + ".<br />To: " + document.getElementById("destination").value + ".<br /> Driving distance: " + result.routes[0].legs[0].distance.text + ".<br />Duration: " + result.routes[0].legs[0].duration.text + ".</div>");
-            endLocationLat = result.routes[0].legs[0].end_location.lat();
-            endLocationLng = result.routes[0].legs[0].end_location.lng();
-            startLocationLat = result.routes[0].legs[0].start_location.lat();
-            startLocationLng = result.routes[0].legs[0].start_location.lng();
-            //  console.log(endLocationLat);
-            //  console.log(endLocationLng);
-            //  console.log (startLocationLat);
-            //  console.log(startLocationLng);
+// myGasFeed API
+var gasLat = p1.lat() + (p2.lat() - p1.lat()) * m;//30.2672; // will call from google
+var gasLng =  p1.lng() + (p2.lng() - p1.lng()) * m; //-97.7431; // will call from google
+var queryURL = "http:/api.mygasfeed.com/stations/radius/" + gasLat + "/" + gasLng + "/5/reg/price/esmbi7wobr.json?";
 
-            //Display route
-            directionsDisplay.setDirections(result);
-        } else {
-            //Delete route from map
-            directionsDisplay.setDirections({
-                routes: []
-            });
-            //Center map in Austin (could center to user)
-            map.setCenter(myLatLng);
-            //show error message
-            $("#output").html("<div class='alert-danger'>Could not retrieve driving distance.</div>");
-        }
-    });
 
+$.get(queryURL).then( data => {
+   console.log(JSON.stringify(data));
+   $(".gasStuff").append("<tr><td>" + data.stations[0].station + "</td><td>" + "$" + data.stations[0].reg_price + "</td><td>" + data.stations[0].address + "</td><td>" + data.stations[0].city + "," + data.stations[0].region + "</td><td>" + data.stations[0].distance + "</td></tr>")
+   //$("#info").text(JSON.stringify(data.stations[0].reg_price));
+   //console.log("country" + data.stations.zip);
+   console.log ("Gas Lat = " + gasLat)
+});
+
+
+  return points;
 }
 
 //Create autocomplete objects for inputs
 var options = {
-    types: ['(cities)']
+  types: ['(cities)']
 }
 
-//---------FUNCTIONS-----------------
+var input1 = document.getElementById("origin");
+var autocomplete1 = new google.maps.places.Autocomplete(input1, options);
 
-// DATE SCRIPT
-let minDate = new Date();
+var input2 = document.getElementById("destination");
+var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
 
-$('#depart').datepicker({
-    showAnim: 'drop',
-    numberOfMonth: 1,
-    minDate: minDate,
-    dateFormat: 'mm/dd/yy',
-    onClose: function (selectedDate) {
-        $('#return').datepicker('option', 'minDate', 'selectedDate');
-    }
-});
-$('#return').datepicker({
-    showAnim: 'drop',
-    numberOfMonth: 1,
-    minDate: minDate,
-    dateFormat: 'mm/dd/yy',
-    onClose: function (selectedDate) {
-        $('#depart').datepicker('option', 'minDate', 'selectedDate');
-    }
-});
 
-//---Round Trip & One Way Button---
-$('.one-way-button').on('click', function () {
-    $('#return').toggle(500);
-});
-$('.round-trip-button').on('click', function () {
-    $('#return').show(500);
-});
+// // myGasFeed API
+// var gasLat = 30.2672; // will call from google
+// var gasLng = -97.7431; // will call from google
+// var queryURL = "http:/api.mygasfeed.com/stations/radius/" + gasLat + "/" + gasLng + "/5/reg/price/esmbi7wobr.json?";
+
+
+// $.get(queryURL).then( data => {
+//    console.log(JSON.stringify(data));
+//    $(".gasStuff").append("<tr><td>" + data.stations[0].station + "</td><td>" + "$" + data.stations[0].reg_price + "</td><td>" + data.stations[0].address + "</td><td>" + data.stations[0].distance + "</td></tr>")
+//    //$("#info").text(JSON.stringify(data.stations[0].reg_price));
+//    //console.log("country" + data.stations.zip);
+// });
+
+
