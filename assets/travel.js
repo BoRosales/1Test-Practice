236ec57b0731c792cc9e26c4d52afb25eee7c962
JAWilmer <39922930+JAWilmer@users.noreply.github.com>
2018-09-25 21:24:38 -0500

    Add files via upload

--- assets/travel.js
+++ assets/travel.js
@@ -1,24 +1,23 @@
+let minDate = new Date();
 
-  let minDate = new Date();
-
-  $('#depart').datepicker({
-      showAnim: 'drop',
-      numberOfMonth: 1,
-      minDate: minDate,
-      dateFormat: 'mm/dd/yy',
-      onClose: function(selectedDate){
-          $('#return').datepicker('option', 'minDate', selectedDate);
-      }
-});
-  $('#return').datepicker({
-      showAnim: 'drop',
-      numberOfMonth: 1,
-      minDate: new Date,
-      dateFormat: 'mm/dd/yy',
-      onClose: function(selectedDate){
-          $('#depart').datepicker('option', 'minDate', selectedDate);
+$('#depart').datepicker({
+  showAnim: 'drop',
+  numberOfMonth: 1,
+  minDate: minDate,
+  dateFormat: 'dd/mm/yy',
+  onClose: function (selectedDate) {
+    $('#depart').datepicker('option', 'minDate', selectedDate);
   }
 });
+// $('#return').datepicker({
+//   showAnim: 'drop',
+//   numberOfMonth: 1,
+//   minDate: new Date,
+//   dateFormat: 'dd/mm/yy',
+//   onClose: function (selectedDate) {
+//     $('#return').datepicker('option', 'minDate', selectedDate);
+//   }
+// });
 
 //MAP SCRIPT
 var endLocationLat;
@@ -54,6 +53,8 @@ function initMap() {
   calculateAndDisplayRoute(directionsService, directionsDisplay);
   var onChangeHandler = function () {
     calculateAndDisplayRoute(directionsService, directionsDisplay);
+    //THIS IS WHERE WE PUT THE FUNCTION TO DISPLAY THE FLIGHTS
+    displayAirInfo(); 
   };
   document.getElementById('searchAround').addEventListener('click', onChangeHandler);
 }
@@ -77,7 +78,7 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
         // console.log("overview path lat/ long coordinates: "+ response.routes[0].overview_path);
         var legs = response.routes[0].legs;
         //Display distance and time in DOM
-        $("#output").html("<div class='alert-info'>From: " + document.getElementById("origin").value + ".<br />To: " + document.getElementById("destination").value + ".<br /> Driving distance: " + response.routes[0].legs[0].distance.text + ".<br />Duration: " + response.routes[0].legs[0].duration.text + ".</div>");
+        $("#drivingCard").html("<div class='alert-info'>From: " + document.getElementById("origin").value + ".<br />To: " + document.getElementById("destination").value + ".<br /> Driving distance: " + response.routes[0].legs[0].distance.text + ".<br />Duration: " + response.routes[0].legs[0].duration.text + ".</div>");
         // startLocationLat = response.routes[0].legs[0].start_location.lat();
         // startLocationLng = response.routes[0].legs[0].start_location.lng();
         // endLocationLat = response.routes[0].legs[0].end_location.lat();
@@ -110,16 +111,18 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
         for (var i = 0; i < gmarkers.length; i++) {
           gmarkers[i].setMap(null);
         }
-        gmarkers = [];
+        // gmarkers = [];
         //321100 = about 200 miles
         var points = polyline.GetPointsAtDistance(321100);
         for (var i = 0; i < points.length; i++) {
           var marker = new google.maps.Marker({
             map: map,
             position: points[i],
-            title: i*200 + 200 + " miles"
+            title: i * 200 + 200 + " miles"
           });
           marker.addListener('click', openInfoWindow);
+          gmarkers.push(marker);
+
         }
       }
     });
@@ -127,7 +130,7 @@ function calculateAndDisplayRoute(directionsService, directionsDisplay) {
 
 google.maps.event.addDomListener(window, 'load', initMap);
 
-// CREATE 200 MILE MARKERS
+// CREATE GAS STOPS EVERY 200 MILES
 function createMarker(latlng, label, html, color) {
   var contentString = '<b>' + label + '</b><br>' + html;
   var marker = new google.maps.Marker({
@@ -191,25 +194,52 @@ google.maps.Polyline.prototype.GetPointsAtDistance = function (miles) {
       points.push(new google.maps.LatLng(p1.lat() + (p2.lat() - p1.lat()) * m, p1.lng() + (p2.lng() - p1.lng()) * m));
       next += miles;
       /////////HHHEEEEEERRRRRRRRREEEEEE ARE THE LAT/ LONG COORDINATES YOU NEED FOR THE GAS API
-      console.log (p1.lat() + (p2.lat() - p1.lat()) * m, p1.lng() + (p2.lng() - p1.lng()) * m)
+      // console.log(p1.lat() + (p2.lat() - p1.lat()) * m, p1.lng() + (p2.lng() - p1.lng()) * m)
     }
   }
 
-// myGasFeed API
-var gasLat = p1.lat() + (p2.lat() - p1.lat()) * m;//30.2672; // will call from google
-var gasLng =  p1.lng() + (p2.lng() - p1.lng()) * m; //-97.7431; // will call from google
-var queryURL = "http:/api.mygasfeed.com/stations/radius/" + gasLat + "/" + gasLng + "/5/reg/price/esmbi7wobr.json?";
+  // myGasFeed API  WORKING CODE
+  var gasLat = p1.lat() + (p2.lat() - p1.lat()) * m;//30.2672; // will call from google
+  var gasLng = p1.lng() + (p2.lng() - p1.lng()) * m; //-97.7431; // will call from google
+  var gasURL = "http:/api.mygasfeed.com/stations/radius/" + gasLat + "/" + gasLng + "/5/reg/price/esmbi7wobr.json?";
 
 
-$.get(queryURL).then( data => {
-   console.log(JSON.stringify(data));
-   $(".gasStuff").append("<tr><td>" + data.stations[0].station + "</td><td>" + "$" + data.stations[0].reg_price + "</td><td>" + data.stations[0].address + "</td><td>" + data.stations[0].city + "," + data.stations[0].region + "</td><td>" + data.stations[0].distance + "</td></tr>")
-   //$("#info").text(JSON.stringify(data.stations[0].reg_price));
-   //console.log("country" + data.stations.zip);
-   console.log ("Gas Lat = " + gasLat)
-});
+  $.get(gasURL).then(data => {
+    //  console.log(JSON.stringify(data));
+    // console.log(data);
 
 
+
+    // $("#drivingCard").html("<div class='alert-info'>From: " + document.getElementById("origin").value + ".<br />To: " + document.getElementById("destination").value + ".<br /> Driving distance: " + response.routes[0].legs[0].distance.text + ".<br />Duration: " + response.routes[0].legs[0].duration.text + ".</div>");
+
+    ///////////////////////////////////
+
+    $("#gasStationsCard").html("<div class='alert-info2'>200 MILES" + "<br />Regular $" + data.stations[0].reg_price + "<br />" + data.stations[0].station + "<br />" + data.stations[0].address + "<br />" + data.stations[0].city + "," + data.stations[0].region + "</div>");
+
+
+
+    // $(".gasStuff").append(data.stations[0].station, data.stations[0].reg_price)
+    // data.stations[0].address + ".<br />", data.stations[0].city + ".<br />", data.stations[0].region + ".<br />", data.stations[0].distance)
+
+
+    // $(".gasStuff").append("<tr><td>" + data.stations[0].station + "</td><td>" + "$" + data.stations[0].reg_price + "</td><td>" + data.stations[0].address + "</td><td>" + data.stations[0].city + "," + data.stations[0].region + "</td><td>" + data.stations[0].distance + "</td></tr>")
+    // $(".gasStuff").append("<tr><td>" + data.stations[1].station + "</td><td>" + "$" + data.stations[1].reg_price + "</td><td>" + data.stations[1].address + "</td><td>" + data.stations[1].city + "," + data.stations[1].region + "</td><td>" + data.stations[1].distance + "</td></tr>")
+    // $(".gasStuff").append("<tr><td>" + data.stations[2].station + "</td><td>" + "$" + data.stations[2].reg_price + "</td><td>" + data.stations[2].address + "</td><td>" + data.stations[2].city + "," + data.stations[2].region + "</td><td>" + data.stations[2].distance + "</td></tr>")
+    //$("#info").text(JSON.stringify(data.stations[0].reg_price));
+    //console.log("country" + data.stations.zip);
+    //$("#info").text(JSON.stringify(data.stations[0].reg_price));
+    //console.log("country" + data.stations.zip);
+    //  console.log ("Gas Lat = " + gasLat)
+
+
+
+
+
+
+
+
+
+  });
   return points;
 }
 
@@ -225,17 +255,75 @@ var input2 = document.getElementById("destination");
 var autocomplete2 = new google.maps.places.Autocomplete(input2, options);
 
 
-// // myGasFeed API
-// var gasLat = 30.2672; // will call from google
-// var gasLng = -97.7431; // will call from google
-// var queryURL = "http:/api.mygasfeed.com/stations/radius/" + gasLat + "/" + gasLng + "/5/reg/price/esmbi7wobr.json?";
+//SKYPICKER API /////////////////////////////////////////
+// var originCity= Boston;
+// var destinationCity = Autin;
+// var dateFrom = 12/12/2018;
+// var dateTo = 24/12/2018;
 
 
-// $.get(queryURL).then( data => {
-//    console.log(JSON.stringify(data));
-//    $(".gasStuff").append("<tr><td>" + data.stations[0].station + "</td><td>" + "$" + data.stations[0].reg_price + "</td><td>" + data.stations[0].address + "</td><td>" + data.stations[0].distance + "</td></tr>")
-//    //$("#info").text(JSON.stringify(data.stations[0].reg_price));
-//    //console.log("country" + data.stations.zip);
+// const airURL = "https://api.skypicker.com/flights?flyFrom=" + originCity + "&to=Austin&dateFrom=24/11/2018&dateTo=24/12/2018&partner=picky";
+// function displayAirInfo () {
+
+//TESTING THESE URLS
+// const airURL = "https://api.skypicker.com/flights?flyFrom=Boston&to=Austin&dateFrom=24/11/2018&dateTo=24/12/2018&partner=picky";
+// var airURL = "https://api.skypicker.com/flights?flyFrom=${originCity}&to=${destinationCity}&dateFrom=${dateFrom}&dateTo=${dateTo}&partner=picky";
+// var airURL = "https://api.skypicker.com/flights?flyFrom=PRG&to=LGW&dateFrom=18/11/2018&dateTo=12/12/2018&partner=picky";
+// var airURL = "https://api.skypicker.com/flights?curr=USD&flyFrom=Boston&to=Austin&dateFrom=24/11/2018&dateTo=24/12/2018&partner=picky";
+
+
+function displayAirInfo () {
+var originCity = document.getElementById("origin").value; 
+var destinationCity = document.getElementById("destination").value; // #destination
+var dateFrom = document.getElementById("depart").value;  
+// var dateTo = document.getElementById("return").value; 
+
+const airURL = "https://api.skypicker.com/flights?curr=USD&typeFlight=round&flyFrom=" + originCity + "&to=" + destinationCity + "&dateFrom=" + dateFrom + "&dateTo=" + dateFrom + "&partner=picky";
+
+// return airURL;
+$.get(airURL).then(response => {
+  
+  console.log("From: " + originCity);
+  console.log("To: " + destinationCity);
+  console.log("Departure Date:  " + dateFrom);
+  // First flight
+  console.log("Flight price: USD $" + response.data[0].price);
+  console.log("Travel Time: " + response.data[0].fly_duration);
+  console.log("Airline: ", response.data[0].airlines);
+  //Second flight
+  console.log("Flight price: USD $" + response.data[1].price);
+  console.log("Travel Time: " + response.data[1].fly_duration);
+  console.log("Airline: ", response.data[1].airlines);
+  //Third flight
+  console.log("Flight price: USD $" + response.data[2].price);
+  console.log("Travel Time: " + response.data[2].fly_duration);
+  console.log("Airline: ", response.data[2].airlines);
+
+});
+}
+
+
+// $.get(airURL).then( response => {
+  //  console.log(response);
+  //  console.log(response.data[0]);
+  //  console.log(response.data[1]);
+  //  console.log(response.data[2]);
+  //  console.log(airURL);
+  //  $(".airStuff").append("<tr><td>" + response.data[0].duration.fly_duration + "</td></tr>")
+  //  + "</td><td>" + "$" + data.stations[0].reg_price + "</td><td>" + data.stations[0].address + "</td><td>" + data.stations[0].city + "," + data.stations[0].region + "</td><td>" + data.stations[0].distance + "</td></tr>")
+
 // });
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 
